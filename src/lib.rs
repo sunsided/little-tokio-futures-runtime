6d51cc5740655d26de86d367af46f032de17cef3
Markus Mayer <widemeadows@gmail.com>
2024-07-10 15:34:44 +0200

    Add code documentation

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,28 @@ use std::time::Duration;
 
 use futures::task::{waker_ref, ArcWake};
 
+/// `Runtime` is a struct that represents a runtime for managing `Task`s concurrently.
 pub struct Runtime {
     tasks: Arc<Mutex<Vec<Arc<Task>>>>,
 }
 
+/// A type representing a task.
+///
+/// The `Task` struct encapsulates a future that can be executed asynchronously.
 struct Task {
     future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
     completed: Mutex<bool>,
 }
 
 impl Runtime {
+    /// Creates a new runtime.
     pub fn new() -> Self {
         Self {
             tasks: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
+    /// Spawns a new task onto the executor.
     pub fn spawn<F>(&self, future: F)
     where
         F: Future<Output = ()> + Send + 'static,
@@ -36,6 +42,11 @@ impl Runtime {
         task.poll();
     }
 
+    /// Runs the task manager in a loop until all tasks are completed.
+    ///
+    /// The `run` method continuously checks the tasks in the task manager and removes
+    /// completed tasks until there are no more tasks remaining. During each iteration,
+    /// it also sleeps for 10 milliseconds to avoid unnecessarily consuming CPU resources.
     pub fn run(&self) {
         loop {
             let mut tasks = self.tasks.lock().unwrap();
@@ -55,9 +66,12 @@ impl Runtime {
 
 impl Task {
     fn poll(self: Arc<Self>) {
+        // Get a waker to the task. Works since it implements ArcWake.
         let waker = waker_ref(&self);
         let mut context = Context::from_waker(&*waker);
 
+        // Poll the task future and flag it as completed when it is ready, so that
+        // it can be removed from the runtime's task vector.
         let mut future = self.future.lock().unwrap();
         if let Poll::Ready(_) = future.as_mut().poll(&mut context) {
             let mut completed = self.completed.lock().unwrap();
@@ -68,9 +82,9 @@ impl Task {
 
 impl ArcWake for Task {
     fn wake_by_ref(arc_self: &Arc<Self>) {
-        let cloned = arc_self.clone();
+        let task = arc_self.clone();
         thread::spawn(move || {
-            cloned.poll();
+            task.poll();
         });
     }
 }
